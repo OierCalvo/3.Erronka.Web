e330dec978b5ec84c3b722edf7182bf1659ddb24
Aimar <aim.zab.jau@goierrieskola.org>
2026-03-12 10:28:56 +0100

    Lehenengo aldaketak

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header">
+            <a href="">Hasiera</a>
+            <a href="">Guri buruz</a>
+            <a href="">Habitatak</a>
+            <a href="">Ekitaldiak</a>
+            <button>Saioa hasi</button>
+        </div>
+    </header>
+</body>
+</html>
