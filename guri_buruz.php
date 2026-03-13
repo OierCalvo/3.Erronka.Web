1131244958e2394d7a6028f48dd416b0aef7e08c
Aimar <aim.zab.jau@goierrieskola.org>
2026-03-13 13:43:08 +0100

    Guri buruz atala hasita

--- guri_buruz.php
+++ guri_buruz.php
@@ -6,10 +6,29 @@ include_once "header.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Guri buruz</title>
     <link rel="stylesheet" href="css.css">
 </head>
 <body>
-    
+    <h2>Zoologikoaren historia eta helburua</h2>
+    <div class="zoo_h">
+    <p>1968 urtean Beasainen natura zale eta hiritar talde bat bildu ziren helburu bakar batekin,
+    edozein adinetako pertsonek euskal herriko eta mundu osoko faunari buruz ikasteko leku bat sortzea,
+    horrela sortu zen BBC zoologikoa.</p>
+    <p>1970-eko hamarkadan zoologikoa haunditzen hasi zen gutxinaka gutxinaka.1973-an hegaztien habitat-a sortu zen, txoritegi irekia penintsula iberikoko hegazti ezberdinekin. 
+    1979-an gure zoologikoko lehenengo ugaztun exotikoak izan genituen, kapibara talde bat eta jaguar talde bat, hauek portugalgo zoologiko batekin egindako truke batean iritsi ziren.
+    1984-an gure lehenengo basabizitza erreskate eta errehabilitazio zentroa sortu genuen, hau albataritza zentro bat bezela funtzionatzen zuen, zaurituta edo gaixotasunak zeuzkaten animaliak honera etortzen dira mota askotako errebisioak eta operazioak egiteko.
+    </p>
+    <h3>Fundatzaileak:</h3>
+    <p><b>Maria Antonia Garmendia</b>: Beasaineneko eskolan irakatsi zuen biologoa, animalien abitatei buruzko ezagutzak zituena.</p>
+    <p><b>Miguel Koldo Iriondo</b>: Zumarragako enpresaburua eta filantropoa, zoologikoa sortzeko diru gehiena eman zuena.</p>
+    <p><b>Jorge Alzaga Orio</b>: Euskal herriko albatari ezagunetarikoena, animaliei sendatzeko eta osasuntsu mantentzeko jakintzak eman eta irakatsi zituena.</p>
+    <br>
+    <h3>Zoologikoaren helburua:</h3>
+    <p>Hasieratik gure filosofia oso argia izan da, lehenik eta behin ingurumen hezkuntza sustatzen dugu jarduera ezberdinen bitartez, hala nola, eskola ezberdinei hitzaldiak eta ateraldiak eskaintzen, baita ere tailer ezberdinak egiten.
+    Horrez gain desagertzeko arriskuan dauden animalien konserbazioa sustatzen dugu programa ezberdinen bitartez. Azkenik zoologikoko animalien ongizatea lehenesten dugu ezerren aurretik, honen adibideak bezela, gure habitat artifizialak ahalik eta errealenak egiten ditugu animaliek naturan egongo balira bezela sentitu daitezen.
+    </p>
+
+    </div>
 </body>
 </html>
