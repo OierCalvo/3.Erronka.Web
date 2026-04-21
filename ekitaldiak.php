4ea5b875c0e44932e344e681f064776cfa05c12f
OierCalvo <oie_cal_arr@goierrieskola.org>
2026-04-21 08:19:49 +0200

    Ekitaldiak atala eta kudeaketa funtzionalitateak gehitu

--- ekitaldiak.php
+++ ekitaldiak.php
@@ -1,5 +1,6 @@
 <?php
 include_once "header.php";
+include_once "db.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +11,21 @@ include_once "header.php";
     <link rel="stylesheet" href="css.css">
 </head>
 <body>
-    
+    <div class="habitat-container">
+    <?php
+        if($_SESSION["_LANGUAGE"] == "eus"){
+        $stmt = $pdo->query("SELECT * FROM ekitaldiak");
+        foreach($stmt as $row){
+            echo "<a href='" . $row["id"] . ".php'>" ;
+            echo "<div>";
+            echo "<h1>" . trans($row['ekitaldi_izena']) . "</h1>";
+            echo "<img src='irudiak/" . $row['irudiak'] . "' />";
+            echo "<h2>" . $row["data"] . "</h2>";
+            echo "<p>" . $row["deskribapena"] . "</p>";
+            echo "</div>";
+            echo "</a>";
+        }
+        }
+    ?>
 </body>
 </html>
